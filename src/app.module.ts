16be52011f3b869a10019ce60ad53d1c8f04026f
dadolyner <skulj.david@gmail.com>
2021-08-12 14:57:30 +0200

    authorization with jwt tokens complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { typeOrmCOnfig } from './config/typeorm.config';
-import { QuoteModule } from './quote/quote.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeOrmCOnfig),
-    AuthorizationModule,
-    QuoteModule,
-  ],
+  imports: [TypeOrmModule.forRoot(typeOrmCOnfig), AuthorizationModule],
   controllers: [],
   providers: [],
 })
